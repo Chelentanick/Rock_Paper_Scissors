0e2cc5be48cf6e6ab0f71cf372ad3d0155d0fd87
Artur Pashchenko <144344189+Chelentanick@users.noreply.github.com>
2024-03-19 15:51:24 +0100

    Say hello to player and add statistic

---
+++ Rock_Paper_Scissors/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors
+{
+    internal class Program
+    {
+        public static void Stat(string name, int age, int rounds, int wins)
+        {
+            Console.WriteLine("_________________________________________________________\n");
+
+            Console.WriteLine($"| Name: {name} | | Age: {age} | | Rounds: {rounds} | | Wins: {wins} |");
+            Console.WriteLine("_________________________________________________________");
+        }
+        static void Main(string[] args)
+        {
+            string playerName = "";
+            int playerAge = 0;
+            int playerRounds = 0;
+            int playerWins = 0;   
+            SayHello.SayHelloToPlayer(ref playerName, ref playerAge);
+
+            Stat(playerName, playerAge, playerRounds, playerWins);
+
+
+
+
+        }
+    }
+}
